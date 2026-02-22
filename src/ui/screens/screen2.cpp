c1867fd0ce84d7e5519dc88e7848787891c39e54
Andreas Lüthi <andreas.luethi@gmx.net>
2026-02-22 15:45:37 +0100

    ui/screens/screen2: updated to display the actual flap value dynamically, replacing the static "FLAPS" label.

--- src/ui/screens/screen2.cpp
+++ src/ui/screens/screen2.cpp
@@ -2,12 +2,25 @@
 
 #include "esp_log.h"
 #include "lvgl.h"
+#include "../../flaputils.hpp"
 
 // Waveshare BSP for this exact board (handles power rails + CO5300 + pins)
 #include "bsp/esp32_s3_touch_amoled_1_75.h"
 
+extern flaputils::FlapSymbolResult get_flap_actual();
+
 static const char* TAG = "screen2";
 static lv_obj_t* s_screen = nullptr;
+static lv_obj_t* s_flap_label = nullptr;
+
+static void ui_update_timer_cb(lv_timer_t* /*t*/)
+{
+    if (s_flap_label)
+    {
+        flaputils::FlapSymbolResult res = get_flap_actual();
+        lv_label_set_text(s_flap_label, res.symbol ? res.symbol : "N/A");
+    }
+}
 
 static void ui_create_screen2()
 {
@@ -15,16 +28,25 @@ static void ui_create_screen2()
     lv_obj_set_style_bg_color(s_screen, lv_color_black(), 0);
     lv_obj_set_style_bg_opa(s_screen, LV_OPA_COVER, 0);
 
-    lv_obj_t* label = lv_label_create(s_screen);
-    lv_label_set_text(label, "FLAPS");
-    lv_obj_set_style_text_color(label, lv_color_white(), 0);
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_48, 0);
-    lv_obj_center(label);
+    // Title
+    lv_obj_t* title = lv_label_create(s_screen);
+    lv_label_set_text(title, "FLAPS");
+    lv_obj_set_style_text_color(title, lv_color_white(), 0);
+    lv_obj_set_style_text_font(title, &lv_font_montserrat_16, 0);
+    lv_obj_align(title, LV_ALIGN_TOP_MID, 0, 10);
+
+    // Dynamic Flap Value
+    s_flap_label = lv_label_create(s_screen);
+    lv_label_set_text(s_flap_label, "N/A");
+    lv_obj_set_style_text_color(s_flap_label, lv_color_white(), 0);
+    lv_obj_set_style_text_font(s_flap_label, &lv_font_montserrat_48, 0);
+    lv_obj_center(s_flap_label);
 }
 
 void screen2_create()
 {
     ui_create_screen2();
+    lv_timer_create(ui_update_timer_cb, 100, nullptr);
 }
 
 lv_obj_t* screen2_get()
