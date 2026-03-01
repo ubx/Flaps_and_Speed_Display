eb8030694d69ac8a00159ef6fceb6a4917acacad
Andreas Lüthi <andreas.luethi@gmx.net>
2026-03-01 09:51:50 +0100

    ui/screens/screen3: Replace slider with a button "+" to increase brightness and a button "-" to decrease brightness. Aligns vertical.

--- src/ui/screens/screen3.cpp
+++ src/ui/screens/screen3.cpp
@@ -5,13 +5,23 @@
 
 // Waveshare BSP for this exact board (handles power rails + CO5300 + pins)
 #include "bsp/esp32_s3_touch_amoled_1_75.h"
+extern const lv_font_t digits_120;
 
 static lv_obj_t* s_screen = nullptr;
 
-static void brightness_slider_event_cb(lv_event_t* e)
+static void brightness_plus_event_cb(lv_event_t* e)
 {
-    lv_obj_t* slider = (lv_obj_t*)lv_event_get_target(e);
-    int brightness = (int)lv_slider_get_value(slider);
+    int brightness = bsp_display_brightness_get();
+    brightness += 10;
+    if (brightness > 100) brightness = 100;
+    bsp_display_brightness_set(brightness);
+}
+
+static void brightness_minus_event_cb(lv_event_t* e)
+{
+    int brightness = bsp_display_brightness_get();
+    brightness -= 10;
+    if (brightness < 30) brightness = 30;
     bsp_display_brightness_set(brightness);
 }
 
@@ -27,19 +37,34 @@ static void ui_create_screen3()
     lv_obj_set_style_text_font(label, &lv_font_montserrat_16, 0);
     lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 10);
 
-    // Brightness Slider
-    lv_obj_t* slider = lv_slider_create(s_screen);
-    lv_obj_set_width(slider, 200);
-    lv_slider_set_range(slider, 30, 100);
-    lv_slider_set_value(slider, bsp_display_brightness_get(), LV_ANIM_OFF);
-    lv_obj_align(slider, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_add_event_cb(slider, brightness_slider_event_cb, LV_EVENT_VALUE_CHANGED, nullptr);
+    // Brightness label
+    lv_obj_t* brightness_label = lv_label_create(s_screen);
+    lv_label_set_text(brightness_label, "Brightness");
+    lv_obj_set_style_text_color(brightness_label, lv_color_white(), 0);
+    lv_obj_set_style_text_font(brightness_label, &lv_font_montserrat_20, 0);
+    lv_obj_align(brightness_label, LV_ALIGN_CENTER, 0, -110);
 
-    lv_obj_t* slider_label = lv_label_create(s_screen);
-    lv_label_set_text(slider_label, "Brightness");
-    lv_obj_set_style_text_color(slider_label, lv_color_white(), 0);
-    lv_obj_set_style_text_font(slider_label, &lv_font_montserrat_16, 0);
-    lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_TOP_MID, 0, -10);
+    // Plus Button
+    lv_obj_t* btn_plus = lv_btn_create(s_screen);
+    lv_obj_set_size(btn_plus, 120, 120);
+    lv_obj_align(btn_plus, LV_ALIGN_CENTER, 0, -40);
+    lv_obj_add_event_cb(btn_plus, brightness_plus_event_cb, LV_EVENT_CLICKED, nullptr);
+
+    lv_obj_t* label_plus = lv_label_create(btn_plus);
+    lv_label_set_text(label_plus, "+");
+    lv_obj_set_style_text_font(label_plus, &digits_120, 0);
+    lv_obj_center(label_plus);
+
+    // Minus Button
+    lv_obj_t* btn_minus = lv_btn_create(s_screen);
+    lv_obj_set_size(btn_minus, 120, 120);
+    lv_obj_align(btn_minus, LV_ALIGN_CENTER, 0, 90);
+    lv_obj_add_event_cb(btn_minus, brightness_minus_event_cb, LV_EVENT_CLICKED, nullptr);
+
+    lv_obj_t* label_minus = lv_label_create(btn_minus);
+    lv_label_set_text(label_minus, "-");
+    lv_obj_set_style_text_font(label_minus, &digits_120, 0);
+    lv_obj_center(label_minus);
 }
 
 void screen3_create()
